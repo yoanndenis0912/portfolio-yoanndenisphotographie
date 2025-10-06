2801087ce448c84e965534da87b322f110650b3e
yoann <yoann@Mac-mini-de-yoann.local>
2025-10-06 08:42:04 +0200

    mise à jour site

--- src/router/AnimatedRoutes.jsx
+++ src/router/AnimatedRoutes.jsx
@@ -1,22 +1,19 @@
-import { Routes, Route, useLocation } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Portfolio from "../pages/Portfolio";
 import Category from "../pages/Category";
 import Contact from "../pages/Contact";
 
 export default function AnimatedRoutes() {
-  const location = useLocation();
-
   return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Home />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/portfolio/:slug" element={<Category />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<Home />} />
-      </Routes>
-    </AnimatePresence>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/portfolio" element={<Portfolio />} />
+      <Route path="/portfolio/:slug" element={<Category />} />
+      <Route path="/contact" element={<Contact />} />
+      {/* route par défaut pour éviter l'écran vide */}
+      <Route path="*" element={<Home />} />
+    </Routes>
   );
 }
